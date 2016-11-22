b2c8e79c2c7bced1b2c3d0744296fc1422b56c07
hoyinng <ho.yin.ng@ryerson.ca>
2016-11-21 20:52:35 -0500

    Add files via upload

---
+++ Archive/2016-11-21-20-49/Block.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Block {
+	int x,y, type;
+	public Block (int x,int y,int shapeType)
+	{
+		this.x=x;
+		this.y=y;
+		this.type = shapeType;
+	}
+	public void draw (Graphics g)
+	{
+		
+	}
+}
---
+++ Archive/2016-11-21-20-49/Game.java
@@ -0,0 +1,79 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Label;
+import java.awt.TextField;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
+
+
+public class Game extends JPanel {
+
+    private int squareX = 50;
+    private int squareY = 50;
+    private int squareW = 20;
+    private int squareH = 20;
+    private int speed = 2000;
+    public Game() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+        
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                moveSquare(0,0);
+            }
+        };
+
+        new Timer(speed, taskPerformer).start();
+        
+
+       
+    }
+    private void moveSquare(int x, int y) {
+        int OFFSET = 1;
+        if ((squareX!=x) || (squareY!=y)) {
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            squareX=x;
+            squareY=y;
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+        } 
+    }
+    
+
+    public Dimension getPreferredSize() {
+        return new Dimension(250,200);
+    }
+    
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+        g.drawString("This is my custom Panel!",10,20);
+        g.setColor(Color.RED);
+        g.fillRect(squareX,squareY,squareW,squareH);
+        g.setColor(Color.BLACK);
+        g.drawRect(squareX,squareY,squareW,squareH);
+    }
+}
