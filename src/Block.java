6b920b55b5addd9f5a4fcafaf66291e2093dff8d
hoyinng <ho.yin.ng@ryerson.ca>
2016-11-25 20:07:00 -0500

    Add files via upload

--- src/Block.java
+++ src/Block.java
@@ -1,15 +1,36 @@
 import java.awt.*;
 
 public class Block {
-	int x,y, type;
-	public Block (int x,int y,int shapeType)
+	int x, y, type, width;
+	Color clr;
+	public Block (int x, int y, int shapeType, int width, Color color)
 	{
 		this.x=x;
 		this.y=y;
+		this.clr = color;
 		this.type = shapeType;
+		this.width = width;
+	}
+	public void rotateCW (){}
+	public void rotateCCW (){}
+	public void moveDown ()
+	{
+		y+=width;
+	}
+	public void moveLeft ()
+	{
+		x-=width;
+		System.out.println("Moving right!");
+	}
+	public void moveRight ()
+	{
+		x+=width;
+		System.out.println("Moving right!");
 	}
 	public void draw (Graphics g)
 	{
-		
+		((Graphics2D) g).setColor(this.clr);
+		((Graphics2D) g).fillRect(this.x,this.y,width,width);
+		System.out.println("X is " + this.x);
 	}
 }
--- src/Map.java
+++ src/Map.java
@@ -1,13 +1,67 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 public class Map {
-	int block_matrix [][]; // 0 : 1 ; false:true for occupation
-	int maxx, maxy;
-	
-	public Map (int maxx, int maxy)
+	private Color color_matrix [][];
+	private int integer_matrix [][];
+	int number_of_squares_x, number_of_squares_y;
+	int block_width = 10;
+	double scale = 0;
+	public Map (int number_of_squares_x, int number_of_squares_y, int block_width)
 	{
-		this.maxx = maxx;
-		this.maxy = maxy;
-		block_matrix = new int [maxx][maxy];
+		this.number_of_squares_x = number_of_squares_x;
+		this.number_of_squares_y = number_of_squares_y;
+		this.block_width = block_width;
+		
+		color_matrix = new Color [number_of_squares_x][number_of_squares_y];
+		integer_matrix = new int [number_of_squares_x][number_of_squares_y];
+		for (int i=0;i<number_of_squares_x;i++){
+			for (int j=0;j<number_of_squares_y;j++){
+				integer_matrix[i][j] = 0;
+				color_matrix[i][j] = Color.orange;
+			}				
+		}
 	}
 	
+	public Color [][] getColorMatrix ()
+	{
+		return color_matrix;
+	}
+	public int [][] getIntegerMatrix ()
+	{
+		return integer_matrix;
+	}
+	public void setMatrix (int index_x, int index_y, Color aColor)
+	{
+		// (TODO) check in bound
+		this.integer_matrix [index_x][index_y] = 1;
+		this.color_matrix[index_x][index_y] = aColor;
+	}
+	public void emptyMatrix (int index_x, int index_y, Color aColor)
+	{
+		setMatrix (index_x,index_y, aColor);
+	}
+	public void draw (Graphics g)
+	{
+		for (int i =0;i<number_of_squares_x;i++)
+		{
+			for (int j=0; j<number_of_squares_y;j++)
+			{
+				if (integer_matrix[i][j] == 0){
+					continue;
+				}
+				Rectangle rect = new Rectangle( 
+						( i * block_width ), (j * block_width),
+						( (i+1) * block_width ), ((j+1) * block_width));
+
+				((Graphics2D) g).setColor(color_matrix[i][j]);
+				((Graphics2D) g).fill (rect);
+				((Graphics2D) g).setColor(Color.black);
+				((Graphics2D) g).draw (rect);
+				
+			}
+		}
+	}
 }
